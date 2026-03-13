f01bffae58581d3d913a0b3978fd1de0c667e9cb
MR VJ <vijaylog0414@gmail.com>
2026-03-13 10:16:44 +0530

    Initial AWS SES POC project

---
+++ ses-backend/src/main/java/com/demo/ses_backend/SesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.demo.ses_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesBackendApplication.class, args);
+	}
+
+}
---
+++ ses-backend/src/test/java/com/demo/ses_backend/SesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.ses_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
